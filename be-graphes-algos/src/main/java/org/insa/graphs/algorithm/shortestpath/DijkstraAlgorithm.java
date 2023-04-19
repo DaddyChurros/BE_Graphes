0c99fbd83c3a621b65237062f9630646b25503df
ImadBKZZ <ied.boukezzata@gmail.com>
2023-04-19 15:29:55 +0200

    Debut Djikstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.algorithm.utils.ElementNotFoundException;
+import org.insa.graphs.algorithm.utils.EmptyPriorityQueueException;
+import org.insa.graphs.model.Arc;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,8 +17,55 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
-        return solution;
+        
+    Label[] label = new Label[data.getGraph().size()];
+    for (int i=0 ; i<data.getGraph().size(); i++){
+        label[i] = new Label(data.getGraph().getNodes().get(i),false, Double.POSITIVE_INFINITY, null);
     }
+
+    label[data.getOrigin().getId()].setCost(0);
+
+    BinaryHeap<Label> heap = new BinaryHeap<>();
+    heap.insert(label[data.getOrigin().getId()]);
+
+    while(heap.isEmpty() && label[data.getDestination().getId()].getMarque() != true){
+        Label current;
+        try{
+            current = heap.deleteMin();
+        }catch(EmptyPriorityQueueException e){
+            break;
+        }
+    label[current.getNode().getId()].setMarque(true);
+
+    for (Arc Successor : data.getGraph().get(current.getNode().getId()).getSuccessors()){
+        if (!data.isAllowed(Successor)){
+            continue;
+        }
+        if(!label[Successor.getDestination().getId()].getMarque()){
+            double costY = label[Successor.getDestination().getId()].getCost();
+            double W = data.getCost(Successor) + label[Successor.getDestination().getId()].getCost();
+            double costX = label[current.getNode().getId()].getCost();
+            
+            if(Double.isFinite(costY) && Double.isFinite(costX+W)){
+                notifyNodeReached(Successor.getDestination());;
+            }
+
+            if(costY > costX + W){
+                label[Successor.getDestination().getId()].setCost(label[Successor.getOrigin().getId()].getCost()+data.getCost(Successor));
+                label[Successor.getDestination().getId()].setPere(current.getNode().getId());
+
+                try{
+                    heap.remove(label[Successor.getDestination().getId()]);
+                    heap.insert(label[Successor.getDestination().getId()]);
+                }catch(ElementNotFoundException e){
+                    heap.insert(label[Successor.getDestination().getId()]);
+                }
+            }
+        }
+    }
+    }
+    return solution;
+    }
+
 
 }
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -4,18 +4,18 @@ import org.insa.graphs.model.Arc;
 
 import org.insa.graphs.model.Node;
 
-public class Label  {
+public class Label implements Comparable<Label> {
 
     private Node current_node;
     private boolean marque;
     private double current_cost;
-    private Arc pere;
+    private int pere;
     
-    public Label(Node current_node){
+    public Label(Node current_node, boolean marque , double current_cost, int pere){
         this.current_node = current_node;
-        this.marque = false;
+        this.marque = marque;
         this.current_cost = Double.POSITIVE_INFINITY;
-        this.pere = null;
+        this.pere = pere;
     }
     
     public Node getNode(){
@@ -25,7 +25,7 @@ public class Label  {
     public double getCost(){
         return this.current_cost;
     }
-    public Arc getPere(){
+    public int getPere(){
         return this.pere;
     }
     public void marquer(){
@@ -38,11 +38,30 @@ public class Label  {
     public void setCost(double cost){
         this.current_cost=cost;
     }
-    public void setPere(Arc pere){
+    public int setPere(int pere){
         this.pere = pere; 
     }
     public void setMarque(boolean Marque){
         this.marque = Marque;
     }
-
+    public int CompareTo(Label label){
+        return Double.compare(this.getCost(), label.getCost());
+    }
 }
+
+
+
+        /* ITERATIONS */
+        //While il existe des sommets non marqués
+        //	x<--ExtractMin(Tas)
+        //	Mark(x)<--true
+        //	For tous les y successeurs de x
+        //		If not Mark(y) then
+        //			Cost(y)<--Min(Cost(y),Cost(x)+W(x,y))
+        //			If Cost(y) a été mis à jour then
+        //				Placer(y, Tas)
+        //				Father(y)<--x
+        //			end if
+        //		end if
+        //	end for
+        //end while
