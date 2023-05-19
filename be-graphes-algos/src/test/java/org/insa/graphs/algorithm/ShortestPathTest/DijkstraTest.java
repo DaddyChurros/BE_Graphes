3d69b12265af42791ecd8a7e2a38fac0f05690ed
ImadBKZZ <ied.boukezzata@gmail.com>
2023-05-19 17:05:10 +0200

    push

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/ShortestPathTest/DijkstraTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/ShortestPathTest/DijkstraTest.java
@@ -2,6 +2,8 @@ package org.insa.graphs.algorithm.ShortestPathTest;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
@@ -9,6 +11,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.lang.model.util.Elements.Origin;
 
 import org.insa.graphs.algorithm.ArcInspectorFactory;
 import org.insa.graphs.algorithm.shortestpath.BellmanFordAlgorithm;
@@ -27,32 +31,102 @@ import org.junit.Test;
 
 
 public class DijkstraTest{
-
-    final static String mapName = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/carre.mapgr";
-    final static String pathName = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31_insa_bikini_canal.path";
-    final static String MapRoutiere = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/Toulouse.mapgr";
-    public static ShortestPathData data;
-    public static BellmanFordAlgorithm bellman;
-    public static DijkstraAlgorithm dijkstra;
-    
+    public static ShortestPathData dataT,dataT2, dataC, dataH,dataB;
+    public static BellmanFordAlgorithm bell;
+   
     public static void initAll() throws Exception {
-        
-        final GraphReader reader = new BinaryGraphReader(
-            new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
 
-        final PathReader pathReader = new BinaryPathReader(
-				new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
+        Node Origine , Destination ,Origine2, Destination2, Origine3, Destination3, Origine4, Destination4;
+
+
+        //Cas Toulouse
+
+        final String Toulouse = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/Toulouse.mapgr";
+
+        final GraphReader readerT = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(Toulouse))));
         
-        data = new ShortestPathData(reader.read(), pathReader.readPath(reader.read()).getOrigin(), pathReader.readPath(reader.read()).getDestination(), ArcInspectorFactory.getAllFilters().get(0));
-        bellman = new BellmanFordAlgorithm(data);
-        dijkstra = new DijkstraAlgorithm(data);
+        Origine = readerT.read().getNodes().get(3605);
+        Destination = readerT.read().getNodes().get(7781);
+
+        
+        dataT = new ShortestPathData(readerT.read(), Origine, Destination, ArcInspectorFactory.getAllFilters().get(0));
+
+        dataT2 = new ShortestPathData(readerT.read(), readerT.read().getNodes().get(7174), readerT.read().getNodes().get(16557), ArcInspectorFactory.getAllFilters().get(0));
+
+
+
+        
+        //Cas Carre
+
+        final String Carre = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/carre-dense.mapgr";
+
+        final GraphReader readerC = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(Carre))));
+
+        Origine2 = readerC.read().getNodes().get(315979);
+        Destination2 = readerC.read().getNodes().get(78657);
+
+        dataC = new ShortestPathData(readerT.read(), Origine2, Destination2, ArcInspectorFactory.getAllFilters().get(0));
+
+        
+
+        
+        //Cas Haute Garonne
+        
+        final String Haute_Garonne = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/haute-garonne.mapgr";
+        
+        final GraphReader readerH = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(Haute_Garonne))));
+        
+        Origine3 = readerH.read().getNodes().get(778);
+        Destination3 = readerH.read().getNodes().get(32568);
+
+
+
+        dataH = new ShortestPathData(readerT.read(), Origine3, Destination3, ArcInspectorFactory.getAllFilters().get(0));
+
+
+
+
+        //Cas bretagne
+
+        final String Bretagne = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/bretagne.mapgr";
+
+        final GraphReader readerB = new BinaryGraphReader(new DataInputStream(new BufferedInputStream(new FileInputStream(Bretagne))));
+
+        Origine4 = readerB.read().getNodes().get(657101);
+        Destination4 = readerB.read().getNodes().get(56068);
+
+
+        dataB = new ShortestPathData(readerT.read(), Origine4, Destination4, ArcInspectorFactory.getAllFilters().get(0));
+
     }
+
+    //Pb à tester chemin inexistant, chemin de longueur nulle, trajet court,trajet long
     
         @Test
 
-        public void TestDijkstra(){
-            //Pb à tester chemin inexistant, chemin de longueur nulle, trajet court,trajet long
-            assertEquals(bellman,dijkstra);;
+        public void TestToulouse(){
+            //algo valide?
+            //assertFalse(new DijkstraAlgorithm(dataT).getSolution().isValid());
+            
+            //Dijkstra renvoie le même resultat que bellman
+            //assertEquals(n,new BellmanFordAlgorithm(dataT).doRun().getPath().getLength(),new DijkstraAlgorithm(dataT).getDoRun().getPath().getLength(),0);
+            
+            //Est ce que Dijkstra renvoie un chemin different
+            assertNotEquals(new DijkstraAlgorithm(dataT), new DijkstraAlgorithm((dataT2)));
+
+
+            
+        }   
+
+        @Test
+
+        public void TestCheminInexistant(){
+             //algo valide?
+           // assertFalse(new DijkstraAlgorithm(dataB).getSolution().isValid());
+            
+            //Dijkstra renvoie le même resultat que bellman
+            //assertFalse(new DijkstraAlgorithm(dataB).getDoRun().isFeasible());
+            
         }
 
 
